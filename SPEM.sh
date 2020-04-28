230138e9b0556a0b645cb6b19d7063a5c86ee58d
root <root@localhost.localdomain>
2020-04-28 11:36:53 +0900

    SPEM 0.1

--- SPEM.sh
+++ SPEM.sh
@@ -1,2 +1,24 @@
 #! /bin/bash
 echo "This is SPEM(Shwan's Papermc Elastic Managment program)"
+echo "You must be in sudoers to run this program"
+echo "You can choose the tool to managment Papermc."
+echo "   "
+echo "1.SPAI(Shwan's Papermc Auto Installer)"
+echo "2.SPAU(Shwan's Papermc Auto Updater)"
+echo "choose program you want to run"
+read pselect [1/2]
+while :; do
+	case $pselect in
+		1)
+			break;;
+		2)
+			break;;
+		*)
+			
+	esac
+	echo "please type 1 or 2"
+	echo "choose program you want to run"
+	read pselect
+done
+
+./"$pselect"/"$pselect".sh
